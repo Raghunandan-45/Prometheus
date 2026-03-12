30733df03c9fdf73e325421f2223bc4a61ceb62d
Raghunandan-45 <araghunandan2015@gmail.com>
2026-03-12 15:18:29 +0530

    Entered raw mode

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
+/*** INCLUDES ***/
 #include <stdio.h>
-#include<ctype.h>
-
+#include <ctype.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 
+/*** DATA ***/
 struct termios orig_termios;// original attributes
 
+/*** TERMINAL ***/
+
+void die(const char *s){
+    perror(s); // returns global error with desc. Also mentiones the string that worked well before the error to give context
+    exit(1); //non-zero value-failure
+}
+
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1){
+        die("txcsetattr");
+    };
 }
 void enableRawMode(){ // typing password in terminal
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){ //Send echo text|main.c || ./main <main.c
+        die("tcgetattr");
+    };
     atexit(disableRawMode); // resets back to original config after terminating the prog by exit() or return from main()
 
     struct termios raw = orig_termios; //copy the struct into raw from orig
@@ -33,8 +46,12 @@ void enableRawMode(){ // typing password in terminal
     raw.c_cc[VMIN] = 0; // min number of bytes for read to return
     raw.c_cc[VTIME] = 10;// max amt of time to wait before read returns. If timeout -> 0 returns. Measured in tenths of sec. We set here 1/10th sec
     raw.c_cc[VLNEXT] = _POSIX_VDISABLE; // In wsl since ctrl +V is binded to pasting in terminal we can't see 22.
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // TCSAFLUSH - tells when to apply the change
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1){
+        die("tcsetattr");
+    }; // TCSAFLUSH - tells when to apply the change
 }
+
+/*** INIT ***/
 
 int main(){
 
@@ -45,7 +62,9 @@ int main(){
     // read(STDIN_FILENO, &c, 1)==1 && c!='q' initially used inside while before adding time attribute
     while(1){ //read 1 byte from i/p into c until no bytes are left
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno!=EAGAIN){
+            die("read");
+        };
         if(iscntrl(c)){ // from ctype; tests whether c is control char(non printable ones)(ASCII 0-31,127)
             printf("%d\r\n",c); // \r - carriage return - necessary for the kernel to move the cursor back to starting of the line, else the program will just print in slanting way. This occurs when we use OPOST.
         }
